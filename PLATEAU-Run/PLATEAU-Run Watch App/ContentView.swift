ad37db154cb71ffedb804dcc24895b767616fb3f
SO 高林秀 <s.takabayashi@super-one.co.jp>
2023-10-24 16:05:03 +0900

    【AppleWatch】試験的に10秒毎にHealthkit APIから歩数を取得しサーバーに投げるコードを追加。

---
+++ PLATEAU-Run/PLATEAU-Run Watch App/ContentView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+import HealthKit
+
+let healthStore = HKHealthStore()
+
+struct ContentView: View {
+    @State private var steps: Int = 0
+    let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect() // 10秒ごとにデータを取得・送信
+
+    var body: some View {
+        VStack {
+            Text("Steps: \(steps)")
+        }
+        .onAppear {
+            requestHealthKitAuthorization()
+        }
+        .onReceive(timer) { _ in
+            fetchStepsData { s in
+                steps = s
+                sendDataToServer(steps: steps)
+            }
+        }
+    }
+
+    func requestHealthKitAuthorization() {
+        let stepType = HKObjectType.quantityType(forIdentifier: .stepCount)!
+        healthStore.requestAuthorization(toShare: [], read: [stepType]) { (success, error) in
+            if !success {
+                // Handle error
+            }
+        }
+    }
+
+    func fetchStepsData(completion: @escaping (Int) -> Void) {
+        let stepsType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+
+        let stepsQuery = HKStatisticsQuery(quantityType: stepsType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, error in
+            let sum = result?.sumQuantity()?.doubleValue(for: HKUnit.count()) ?? 0
+            completion(Int(sum))
+        }
+
+        healthStore.execute(stepsQuery)
+    }
+
+    func sendDataToServer(steps: Int) {
+        let url = URL(string: "YOUR_SERVER_URL")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let parameters: [String: Any] = ["steps": steps]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error sending data: \(error)")
+            }
+        }.resume()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PLATEAU-Run/PLATEAU-Run Watch App/PLATEAU_RunApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLATEAU_RunApp.swift
+//  PLATEAU-Run Watch App
+//
+//  Created by 高林秀 on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PLATEAU_Run_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
