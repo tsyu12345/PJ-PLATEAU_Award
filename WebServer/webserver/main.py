1f8043e74c659996be219516b840298e53a9e5b7
tsyu12345 <syukut@gmail.com>
2023-10-24 19:59:42 +0900

    【Webサーバ】FastAPIによるテスト構築。歩行データのPOSTを作成。

--- WebServer/webserver/main.py
+++ WebServer/webserver/main.py
@@ -1,1 +1,21 @@
+"""サーバーエントリポイント"""
+import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class ActivityData(BaseModel):
+    energy: float
+    distance: float
+    steps: int
+
+@app.post("/receive_data")
+async def receive_data(data: ActivityData):
+    # ここでデータを処理する
+    print(data.energy, data.distance, data.steps)
+    return {"message": "Data received successfully"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
