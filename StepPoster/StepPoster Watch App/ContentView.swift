2cc6e70bd1fbbf4c76182f4662579a3c3210e861
SO 高林秀 <s.takabayashi@super-one.co.jp>
2023-10-26 20:35:45 +0900

    【WatchOS】Healthkit APIから１秒ごとに歩数を取得しサーバーへPOSTするテストの追加。

--- StepPoster/StepPoster Watch App/ContentView.swift
+++ StepPoster/StepPoster Watch App/ContentView.swift
@@ -12,8 +12,8 @@ let healthStore = HKHealthStore()
 
 struct ContentView: View {
     @State private var steps: Int = 0
-    let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect() // 10秒ごとにデータを取得・送信
-
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
             Text("Steps: \(steps)")
@@ -56,17 +56,28 @@ struct ContentView: View {
         let url = URL(string: "http://0.0.0.0:8000/receive_data")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
         let parameters: [String: Any] = ["steps": steps]
-        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
-        print("Conecting your seaver \(url)")
-        print("send data is :\(parameters)" )
-        URLSession.shared.dataTask(with: request) {
-            (data, response, error) in
-            if let error = error {
-                print("Error sending data: \(error)")
-            }
-        }.resume()
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters)
+            print("Connecting to your server \(url)")
+            print("send data is :\(parameters)")
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Error sending data: \(error)")
+                }
+                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                    print("Server responded with status code: \(httpResponse.statusCode)")
+                }
+            }.resume()
+        } catch {
+            print("Error serializing JSON: \(error)")
+        }
     }
+
 }
 
 
