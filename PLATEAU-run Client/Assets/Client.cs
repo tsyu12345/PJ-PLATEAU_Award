8699b4b05db5ffdccb6b2624b43c18feec98df65
tsyu1234 <syukunt@gmail.com>
2023-11-01 00:51:10 +0900

    【Unity Client】UnityWebRequestを利用したHTTP通信の実装中。試験的にGetメソッド呼び出しを追加。

--- PLATEAU-run Client/Assets/Client.cs
+++ PLATEAU-run Client/Assets/Client.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Client : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+enum HTTPMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
+
+/***
+* UnityWebRequestを使用し、サーバーから色々な情報を取得するクラス
+*/
+public class Client : MonoBehaviour {
+
+    [Header("Server Info")]
+    public string baseURL = "http://localhost:8080/";
+    public string step_endpoint = "/step_data";
+    public string userid = "TESTUSER1";
+    
+
+    void Start() {
+        //Nope
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        //Nope
     }
+
+
+    public int GetSteps() {
+        string url = baseURL + step_endpoint;
+        
+        using(var request = UnityWebRequest.Get(url)) {
+            yield return request.SendWebRequest();
+            if(request.isHttpError || request.isNetworkError) {
+                Debug.Log(request.error);
+            } else {
+                Debug.Log(request.downloadHandler.text);
+                //todo:レスポンスからステップ数を取得
+            }
+        }         
+    }
+
 }
