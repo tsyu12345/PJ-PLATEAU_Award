a8d7a0b16c6f94aa9e0b44f37c3450a8d18d49c6
tsyu12345 <syukut@gmail.com>
2023-12-06 20:20:36 +0900

    【開発環境】poetry scriptsの修正

--- PushLFS.ps1
+++ PushLFS.ps1
@@ -3,12 +3,20 @@ param(
     [string]$DestinationDirectory
 )
 
+# 7Zipの実行ファイルへのパス（必要に応じて変更してください）
+$7ZipPath = "C:/Program Files/7-Zip/7z.exe"
+
 # ZIPファイルの名前を生成（現在のタイムスタンプを使用）
 $timestamp = Get-Date -Format "yyyyMMddHHmmss"
 $zipFileName = "Archive_$timestamp.zip"
+$zipFilePath = Join-Path $DestinationDirectory $zipFileName
 
-# 指定されたファイルをZIP形式で圧縮
-Compress-Archive -Path $FilesToZip -DestinationPath $zipFileName
+# ファイルの一時リストを作成
+$tempFileList = "./filelist.txt"
+$FilesToZip | Out-File $tempFileList
 
-# ZIPファイルを指定されたディレクトリに転送
-Move-Item -Path $zipFileName -Destination $DestinationDirectory
+# 7Zipを使用してファイルをZIP形式で圧縮
+& $7ZipPath a -tzip $zipFilePath $FilesToZip -scsUTF-8
+
+# 一時ファイルリストを削除
+Remove-Item $tempFileList
