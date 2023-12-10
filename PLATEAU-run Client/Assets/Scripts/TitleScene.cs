eb17e03e849e655da521725f131fbb25cd92f1a6
tsyu1234 <syukunt@gmail.com>
2023-12-10 15:06:40 +0900

    【Unity】タイトル➞ロビー間を非同期で切り替え、ローディング画面を挟んだ。

--- PLATEAU-run Client/Assets/Scripts/TitleScene.cs
+++ PLATEAU-run Client/Assets/Scripts/TitleScene.cs
@@ -10,9 +10,24 @@ public class TitleScene : MonoBehaviour {
     public Button startButton;
     private static Camera mainCamera;
 
+    // ロードするシーンの名前
+    public string sceneName;
+
+    // ロードの進捗状況を表示するUIなど
+    public GameObject loadingUI;
+
+    // ロードの進捗状況を管理するための変数
+    private AsyncOperation async;
+
+    // ロードを開始するメソッド
+    public void StartLoad() {
+        StartCoroutine(Load());
+    }
+
     void Start() {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         startButton.onClick.AddListener(moveLobbyScene);
+        loadingUI.SetActive(false);
     }
 
     
@@ -23,6 +38,23 @@ public class TitleScene : MonoBehaviour {
 
     private void moveLobbyScene() {
         Debug.Log("MoveLobbyScene");
-        SceneManager.LoadScene("Lobby");
+        StartCoroutine(Load());
+    }
+
+    // コルーチンを使用してロードを実行するメソッド
+    private IEnumerator Load() {
+        // ロード画面を表示する
+        loadingUI.SetActive(true);
+
+        // シーンを非同期でロードする
+        async = SceneManager.LoadSceneAsync(sceneName);
+
+        // ロードが完了するまで待機する
+        while (!async.isDone) {
+            yield return null;
+        }
+
+        // ロード画面を非表示にする
+        loadingUI.SetActive(false);
     }
 }
