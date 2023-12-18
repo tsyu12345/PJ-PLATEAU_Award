9caebe822a9971db267789661be4f94887b49cd1
tsyu12345 <syukut@gmail.com>
2023-12-18 17:42:45 +0900

    【Unity】ボタンの選択音源を追加。コース選択画面で、カードを一度選択するように変更

--- PLATEAU-run Client/Assets/Scripts/CourseSelect.cs
+++ PLATEAU-run Client/Assets/Scripts/CourseSelect.cs
@@ -11,10 +11,19 @@ public class CourseSelect : MonoBehaviour {
     public GameObject[] Courses;
     public string[] courseNames;
     public GameObject loadingUI;
+    public Button SelectButton;
+    public AudioClip cardSelectSE;
+    public AudioClip selectBtnSE;
+    private int nowSelectedCourseIdx;
+    private string nowSelectCourseName;
     [SerializeField] private Slider _slider;
-    [SerializeField] private AudioSource _audioSource;
+    private AudioSource _audioSource;
     void Start() {
+        nowSelectedCourseIdx = 0;
+        nowSelectCourseName = courseNames[0];
         EventTrigger trigger = closeButton.GetComponent<EventTrigger> ();
+        _audioSource = GetComponent<AudioSource>();
+
         EventTrigger.Entry entry = new EventTrigger.Entry ();
         entry.eventID = EventTriggerType.PointerClick;
         entry.callback.AddListener((BaseEventData eventData)=>{
@@ -28,27 +37,43 @@ public class CourseSelect : MonoBehaviour {
             Debug.LogError("[CourseSelect]courseNames.Length != Courses.Length");
         }
 
+        SelectButton.onClick.AddListener(()=>{
+            _audioSource.PlayOneShot(selectBtnSE);
+            loadingUI.SetActive(true);
+            StartCoroutine(LoadCourse(nowSelectCourseName));
+        });
+        AddClickCardEvent();
+
+    }
+
+
+    private void AddClickCardEvent() {
         for(int courseId = 0; courseId < Courses.Length; courseId++) {
             var course = Courses[courseId];
             EventTrigger courseTrigger = course.GetComponent<EventTrigger> ();
             EventTrigger.Entry courseEntry = new EventTrigger.Entry ();
             courseEntry.eventID = EventTriggerType.PointerClick;
             courseEntry.callback.AddListener((BaseEventData eventData)=>{
-                var courseScene = courseNames[courseId-1];
-                StartCoroutine(LoadCourse(courseScene));
+                Debug.LogWarning("Index" + courseId);
+                Debug.LogWarning("now" + nowSelectedCourseIdx);
+                _audioSource.PlayOneShot(cardSelectSE);
+                if(nowSelectedCourseIdx == courseId-1) {
+                    return;
+                }
+                nowSelectedCourseIdx = courseId-1;
+                nowSelectCourseName = courseNames[courseId-1];
             });
             courseTrigger.triggers.Add(courseEntry);
+            if(courseId == 0) {
+                Outline outline = course.gameObject.AddComponent<Outline>();
+                outline.effectColor = Color.red;
+                outline.effectDistance = new Vector2(3, -3);
+            }
         }
     }
 
 
-    private void OnSelectCourse() {
-        Debug.Log("Select Course");
-    }
-
     private IEnumerator LoadCourse(string sceneName) {
-
-        loadingUI.SetActive(true);
         var courseLoad = SceneManager.LoadSceneAsync(sceneName);
         while(!courseLoad.isDone) {
             _slider.value = courseLoad.progress;
